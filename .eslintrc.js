5711c59814e579d40bb5f0b3a47792d39f19926c
ym87498510 <87498510@qq.com>
2016-11-22 00:37:53 +0800

    add source

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,21 @@ module.exports = {
     sourceType: 'module'
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',//继承标准规则
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
   // add your custom rules here
   'rules': {
-    //为0表示忽略某个检查
-    // allow paren-less arrow functions,允许箭头函数前面不些括号
+    // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    //开发环境允许debug,生产环境则无
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 下面这句是强制要加分号，我不需要
+    // 'semi':['error','always']
+    //
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-// 其实也可以写'./App.vue'
 
+// 通常new对象要复制给某个对象，下面这句的注释意思是此句提跳过语法检查
 /* eslint-disable no-new */
 new Vue({
-  // 挂载点elem,对应到body
   el: 'body',
-  // 注册一个组件,对应到上面引入的近来的App
-  components: { App }
+  components: {App}
 })
