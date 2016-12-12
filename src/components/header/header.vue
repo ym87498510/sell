5ada44c36ba622c89b9a6e15549dbaf30c4fba20
ym87498510 <87498510@qq.com>
2016-12-12 23:03:14 +0800

    增加了header的样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -27,6 +27,9 @@
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 <script type='text/ecmascript-6'>
@@ -47,8 +50,10 @@
   @import "../../common/stylus/mixin.styl"
 
   .header
+    position relative
+    overflow hidden
     color #fff
-    background #999
+    background rgba(7, 17, 27, 0.5)
     .content-wrapper
       position relative
       padding 24px 12px 18px 24px
@@ -133,7 +138,7 @@
       .bulletin-title
         display inline-block
         vertical-align top
-        margin-top 7px
+        margin-top 8px
         width 22px
         height 12px
         bg-image('bulletin')
@@ -148,5 +153,15 @@
         font-size 10px
         right 12px
         top 8px
+
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      /*10像素模糊效果*/
+      filter blur(10px)
 
 </style>
