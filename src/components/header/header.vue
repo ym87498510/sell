7bd3b4489bd07a392ab44673ae06909607bc42c9
ym87498510 <87498510@qq.com>
2016-12-13 23:42:30 +0800

    Sticky footer layout

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,7 +18,7 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" @click="showDetail" class="support-count">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
@@ -30,6 +30,16 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 <script type='text/ecmascript-6'>
@@ -38,6 +48,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
       }
     },
     created() {
@@ -163,5 +183,35 @@
       z-index -1
       /*10像素模糊效果*/
       filter blur(10px)
+
+    .detail
+      position fixed
+      z-indx 100
+      top 0
+      left 0
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        /*最小是视窗的高度*/
+        min-height 100%
+        width 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto 0
+        clear both
+        font-size 32px
+
 
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="item in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    // 接受两个参数
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size
+      }
+    }
+  };
+
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .star
+    font-size 0
+    .star-item
+      display inline-block
+      background-repeat no-repeat
+    &.star-48
+    &.star-36
+    &.star-24
+
+</style>
