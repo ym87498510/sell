7420e02e3efa3ba88e0d496286fe4ea1c4804f13
ym87498510 <87498510@qq.com>
2017-02-05 19:31:29 +0800

    详情页完成

---
+++ src/common/js/date.js
@@ -0,0 +1,24 @@
+export function formatDate(date, fmt) {
+  if (/(y+)/.test(fmt)) {
+    // 匹配y+，替换成年份，转化成字符串，并按y但个数从后面截取
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str))
+    }
+  }
+  return fmt
+}
+// 拼完后截取最后但两位
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length)
+}
--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -16,20 +16,54 @@
         <div class="price">
           <span class="now">¥{{food.price}}</span><span class="old" v-show='food.oldPrice'>¥{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div class="buy" transition="fade" @click.stop.prevent="addFirst" v-show="!food.count || food.count===0">加入购物车
+        </div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{food.info}}</p>
       </div>
-      <div class="buy" transition="fade" @click.stop.prevent="addFirst" v-show="!food.count || food.count===0">加入购物车</div>
+      <split></split>
+      <div class="rating">
+        <h1 class="title">商品评价</h1>
+        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                      :ratings="food.ratings"></ratingselect>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" class="rating-item border-1px"
+                v-for="rating in food.ratings">
+              <div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img :src="rating.avatar" class="avatar" width="12" height="12">
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  // export default 的引入不需要大括号
   import BScroll from 'better-scroll';
+  // 但如果输出了多个函数或变量，就可以用结构的方式来引入
+  import {formatDate} from 'common/js/date'
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect';
   import Vue from 'vue';
 
+  const ALL = 2; // 表示全部
   export default {
 //      接受一个参数food
     props: {
@@ -39,12 +73,20 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: false,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
 //        bscroll帮定给需要在某个范围内滚动的容器
         this.$nextTick(() => {
           if (!this.scroll) {
@@ -66,15 +108,51 @@
 //        在添加第一个的时候出发动画，但由于点击后按钮被隐藏，没法获取按钮的位置，小球位置会有问题，所以添加动画，延迟按钮消失
         this.$dispatch('cart.add', event.target)
         Vue.set(this.food, 'count', 1)
+      },
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      }
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type
+//        更改后刷新滚动位置，保证下拉的回弹位置正确
+//        因为vue的dom更新是异步的，并不一定能取到最新的dom位置，所以要在下一步的异步操作中刷新
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy年MM月dd日 hh时mm分')
       }
     },
     components: {
-      cartcontrol
+      cartcontrol, split, ratingselect
+    },
+    event() {
+
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .food
     position fixed
     left 0
@@ -111,6 +189,7 @@
           color #fff
 
     .content
+      position relative
       padding 18px
       .title
         line-height 14px
@@ -139,26 +218,87 @@
           text-decoration line-through
           font-size 10px
           color rgb(147, 153, 159)
-    .cartcontrol-wrapper
-      position absolute
-      right 12px
-      bottom 12px
-    .buy
-      position absolute
-      right 18px
-      bottom 18px
-      z-index 10
-      height 24px
-      line-height 24px
-      padding 0 12px
-      box-sizing border-box
-      border-radius 12px
-      font-size 10px
-      color #fff
-      background rgb(0, 160, 220)
-      &.fade-transition
-        transition all 0.2s
-        opacity 1
-      &.fade-enter, &.fade-leave
-        opacity 0
+      .cartcontrol-wrapper
+        position absolute
+        right 12px
+        bottom 12px
+      .buy
+        position absolute
+        right 18px
+        bottom 18px
+        z-index 10
+        height 24px
+        line-height 24px
+        padding 0 12px
+        box-sizing border-box
+        border-radius 12px
+        font-size 10px
+        color #fff
+        background rgb(0, 160, 220)
+        &.fade-transition
+          transition all 0.2s
+          opacity 1
+        &.fade-enter, &.fade-leave
+          opacity 0
+    .info
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .text
+        line-height 24px
+        padding 0 8px
+        font-size 12px
+        color rgb(77, 85, 93)
+    .rating
+      padding-top 18px
+      .title
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7, 17, 27, 0.1))
+          .user
+            position absolute
+            right 0
+            top 16px
+            font-size 0
+            line-height 12px
+            .name
+              display inline-block
+              margin-right 6px
+              vertical-align top
+              font-size 10px
+              color rgb(147, 153, 159)
+            .avatar
+              border-radius 50%
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7, 17, 27)
+            .icon-thumb_up, .icon-thumb_doen
+              margin-right 4px
+              line-height 16px
+              font-size 12px
+            .icon-thumb_up
+              color rgb(0, 160, 220)
+            .icon-thumb_down
+              color rgb(147, 153, 159)
+
+        .no-rating
+          padding 16px 0
+          font-size 12px
+          color rgb(147, 153, 159)
 </style>
